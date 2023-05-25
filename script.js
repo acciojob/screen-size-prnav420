89b64ee13848741fcc8e59d0a34c7b21c7e0804c
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-05-25 21:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const div = document.createElement("div");
+div.id = "sizeInfo";
+
+const h1element = document.createElement("h1");
+h1element.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+document.body.append(div);
+div.appendChild(h1element);
+
+window.onresize = function () {
+  h1element.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+};
